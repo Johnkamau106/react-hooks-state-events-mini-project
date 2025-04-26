d796733beabe9e3804614bf34dbd5dfe18bd6cb1
johnkamau106 <john.kamau7@student.moringaschool.com>
2025-04-26 13:43:17 +0300

    added the delete button and the filter accordimg to the category of task

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,20 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, selectedCategory, onCategoryClick }) {
+  const buttons = categories.map((category) => (
+    <button
+      key={category}
+      className={selectedCategory === category ? "selected" : ""}
+      onClick={() => onCategoryClick(category)}
+    >
+      {category}
+    </button>
+  ));
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {buttons}
     </div>
   );
 }
